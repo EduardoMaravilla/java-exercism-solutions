9aaec0347dac60415e4b2e11d1e1aa889108b449
Eduardo Maravilla <130570452+EduardoMaravilla@users.noreply.github.com>
2024-02-18 10:44:09 -0600

    Isogram

--- isogram/IsogramChecker.java
+++ isogram/IsogramChecker.java
@@ -1,10 +1,8 @@
 class IsogramChecker {
-
     boolean isIsogram(String phrase) {
-        phrase = phrase.replaceAll("[ -]", "");        
+        phrase = phrase.replaceAll("[ -]", "");
         int control = 0;
         int longitud = phrase.length();
-
         for (int i = 1; i <= longitud; i++) {
             for (int j = i + 1; j <= longitud; j++) {
                 if (Character.toLowerCase(phrase.charAt(i-1)) == Character.toLowerCase(phrase.charAt(j-1))) {
@@ -16,10 +14,6 @@ class IsogramChecker {
                 break;
             }
         }
-        if (control > 0) {
-            return false;
-        } else {
-            return true;
-        }
+        return control <= 0;
     }
 }
