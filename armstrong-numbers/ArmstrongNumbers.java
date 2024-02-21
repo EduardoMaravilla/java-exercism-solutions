3215d8237c172ba09ed216dbb5e46beebba49cbb
Eduardo Maravilla <eduardomaravillag@gmail.com>
2024-02-20 18:35:47 -0600

    Armstrong Numbers

--- armstrong-numbers/ArmstrongNumbers.java
+++ armstrong-numbers/ArmstrongNumbers.java
@@ -1,13 +1,11 @@
 class ArmstrongNumbers {
-
     boolean isArmstrongNumber(int numberToCheck) {
         double suma=0;
         String number=Integer.toString(numberToCheck);
         String[] num=number.split("");
         int tam=num.length;
-        for (String s : num) {
-            int valor = Integer.parseInt(s);
-            suma = suma + Math.pow(valor, tam);
+        for (String s : num) {            
+            suma = suma + Math.pow(Integer.parseInt(s), tam);
         }
         return suma == (double) numberToCheck;
     }
