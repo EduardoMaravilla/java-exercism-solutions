f9fcdc49cda3eeecb12a95e0ef896e0ec8223a30
Eduardo Maravilla <eduardomaravillag@gmail.com>
2024-02-27 17:20:34 -0600

    Beer Song

--- beer-song/BeerSong.java
+++ beer-song/BeerSong.java
@@ -5,22 +5,18 @@ public class BeerSong {
         }
         int control = verseNumber;
         StringBuilder song = new StringBuilder();
-
         for (int i = 0; i < versenext; i++) {
             song.append(versos(control));
             control--;
         }
         return song.toString();
     }
+
     String versos(int verse) {
         return (verse == 0 ? "No more":verse) + " bottle" + ((verse == 1) ? "" : "s") + " of beer on the wall, " + (verse == 0 ? "no more":verse) + " bottle" + ((verse == 1) ? "" : "s") + " of beer.\n" +
                 ( verse == 0 ?"Go to the store and buy some more, " : "Take " + (verse - 1 == 0 ? "it" : "one") + " down and pass it around, ") + ((verse - 1) > 0 ? (verse-1): ((verse - 1) < 0 ? 99 : "no more")) + " bottle" + ((verse - 1) == 1 ? "" : "s") + " of beer on the wall.\n\n";
     }
-    String singSong() {
-        StringBuilder canc = new StringBuilder();
-        for (int j = 99; j >= 0; j--) {
-            canc.append(versos(j));
-        }
-        return canc.toString();
+    String singSong() {        
+        return sing(99,100);
     }
 }
